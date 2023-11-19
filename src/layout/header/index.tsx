5101696e86d60ffad5bd0d300ac2f718660d98c9
Dionatas1114 <sem.dionatas@gmail.com>
2023-11-18 23:17:47 -0300

    implement switch component

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,39 +1,39 @@
 import { Box, AppBar, Toolbar, IconButton, Typography } from '@mui/material';
 import { Menu as MenuIcon, Search as SearchIcon } from '@mui/icons-material';
-import { SearchIconWrapper, StyledInputBase, Search } from './style';
+import { SearchIconWrapper, StyledInputBase, Search, Children } from './style';
+import SwitchTheme from 'components/switchTheme';
 
-const Header = ({ children }: ChildrenProps) => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-            children={<MenuIcon />}
-          />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-            children={'Financial-UI'}
-          />
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          {children}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
+const Header = ({ children }: ChildrenProps) => (
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+          children={<MenuIcon />}
+        />
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ display: { xs: 'none', sm: 'block' } }}
+          children={'Financial-UI'}
+        />
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+        </Search>
+        <Box sx={{ flexGrow: 1 }} />
+        <Children children={children} />
+        <SwitchTheme />
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export default Header;
