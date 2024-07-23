e599598abc72feca4b7566728ee0bf86afb23f6f
Dionatas1114 <sem.dionatas@gmail.com>
2024-07-22 22:09:06 -0300

    fix env var

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { ResponseType } from 'axios';
 
 const { VITE_MEDIA_API_URL, VITE_BRAPI_API_URL, VITE_CRYPTO_COMPARE_API_URL } = import.meta.env;
 
-const mediaBaseURL = VITE_MEDIA_API_URL || 'http://localhost:8080';
+const mediaBaseURL = VITE_MEDIA_API_URL || 'https://media-download.onrender.com';
 const brapiBaseURL = VITE_BRAPI_API_URL || 'https://brapi.dev/';
 const cryptoCompareBaseURL = VITE_CRYPTO_COMPARE_API_URL || 'https://min-api.cryptocompare.com/';
 
