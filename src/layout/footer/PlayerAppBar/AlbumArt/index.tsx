823292b1a8d9e4c1806bbb34f541ceffcbd62917
pc <sem.dionatas@gmail.com>
2025-04-21 23:00:58 -0300

    fix type Song

--- src/layout/footer/PlayerAppBar/AlbumArt/index.tsx
+++ src/layout/footer/PlayerAppBar/AlbumArt/index.tsx
@@ -1,13 +1,15 @@
-import { CardMedia } from '@mui/material';
+import { Box, CardMedia } from '@mui/material';
 
 const AlbumArt = ({ image }: { image: string }) => {
   return (
-    <CardMedia
-      component="img"
-      sx={{ width: 40, height: 40, borderRadius: '10%' }}
-      image={image}
-      alt="album image"
-    />
+    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+      <CardMedia
+        component="img"
+        sx={{ width: 40, height: 40, borderRadius: '10%' }}
+        image={image}
+        alt="album image"
+      />
+    </Box>
   );
 };
 
